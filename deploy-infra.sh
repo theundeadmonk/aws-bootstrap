849cc52d0f07b25dc6dee3728f09b268bd6d40a4
Aditya Mattos <adityamattos@hey.com>
2022-01-16 13:41:00 -0500

    Add a second instance

--- deploy-infra.sh
+++ deploy-infra.sh
@@ -34,7 +34,7 @@ aws cloudformation deploy \
     --no-fail-on-empty-changeset \
     --capabilities CAPABILITY_NAMED_IAM \
     --parameter-overrides \
-      EC2InstanceType=$EC2InstanceType \
+      EC2InstanceType=$EC2_INSTANCE_TYPE \
       GitHubOwner=$GH_OWNER \
       GitHubRepo=$GH_REPO \
       GitHubBranch=$GH_BRANCH \
@@ -45,5 +45,5 @@ aws cloudformation deploy \
 if [ $? -eq 0 ]; then
     aws cloudformation list-exports \
         --profile awsbootstrap \
-        --query "Exports[?Name=='InstanceEndpoint'].Value"
+        --query "Exports[?starts_with(Name,'InstanceEndpoint')].Value"
 fi
